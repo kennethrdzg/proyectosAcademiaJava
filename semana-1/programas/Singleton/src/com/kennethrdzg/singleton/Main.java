f1319e85099948f50e4fbc591e771f1f382100cb
Kenneth Rodriguez <kenneth.rodriguez.developer@gmail.com>
2024-08-15 18:33:28 -0600

    Clase Main: Crea singleton del Planificador, agenda tareas y las ejecuta

--- semana-1/programas/Singleton/src/com/kennethrdzg/singleton/Main.java
+++ semana-1/programas/Singleton/src/com/kennethrdzg/singleton/Main.java
@@ -7,5 +7,22 @@ enum Prioridad{
 }
 
 public class Main {
-
+	public static void main(String[] args) {
+		Tarea t1 = new Tarea("Abrir Navegador");
+		Planificador planificador = Planificador.getInstancia();
+		planificador.agendarTarea(t1, Prioridad.BAJA);
+		planificador.agendarTarea(new Tarea("Crear Respaldo"), Prioridad.ALTA);
+		planificador.agendarTarea(new Tarea("Actualizar software"),  Prioridad.ALTA);
+		
+		planificador.ejecutarTarea();
+		
+		planificador.agendarTarea(new Tarea("Conectar a Red"),  Prioridad.MEDIA);
+		
+		planificador.ejecutarTarea();
+		
+		planificador.agendarTarea(new Tarea("Crear archivo 'documento.txt'"), Prioridad.BAJA);
+		planificador.agendarTarea(new Tarea("Cerrar Navegador"), Prioridad.BAJA);
+		
+		planificador.ejecutarTodo();
+	}
 }
